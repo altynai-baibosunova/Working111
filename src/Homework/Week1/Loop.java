dfbf9ea2c9ce02c5e81214b6f03b300439a81fcd
Altynai <harmoni5413@gmail.com>
2025-03-25 14:09:48 -0400

    Loop

--- src/Homework/Week1/Loop.java
+++ src/Homework/Week1/Loop.java
@@ -13,6 +13,9 @@ import java.util.regex.*;
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
             int N = Integer.parseInt(bufferedReader.readLine().trim());
+            for(int i=1; i<=10; i++){
+                System.out.println(N +" x "+i+ " = " +(N*i));
+            }
 
             bufferedReader.close();
         }
